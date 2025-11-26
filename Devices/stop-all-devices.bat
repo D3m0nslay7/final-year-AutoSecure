53a46a8545e20298baa08d7e52aea9b69125b1c5
Umer Mohammed <220181356@aston.ac.uk>
2025-11-26 14:18:46 +0000

    fixed the stopping all devices issue in docker setup

--- Devices/stop-all-devices.bat
+++ Devices/stop-all-devices.bat
@@ -8,13 +8,13 @@ echo.
 
 REM Stop all running containers
 echo Stopping containers...
-for /f "tokens=*" %%i in ('docker ps -q --filter name=autosecure-') do (
+for /f "tokens=*" %%i in ('docker ps -q --filter "name=autosecure-"') do (
     docker stop %%i >nul 2>&1
 )
 
 echo.
 echo Removing containers...
-for /f "tokens=*" %%i in ('docker ps -aq --filter name=autosecure-') do (
+for /f "tokens=*" %%i in ('docker ps -aq --filter "name=autosecure-"') do (
     docker rm %%i >nul 2>&1
 )
 
@@ -24,6 +24,6 @@ echo            All AutoSecure Devices Stopped
 echo ============================================================
 echo.
 echo Remaining containers:
-docker ps --filter name=autosecure- --format "  - {{.Names}}" 2>nul
+docker ps --filter "name=autosecure-" --format "  - {{.Names}}" 2>nul
 if errorlevel 1 echo   (None)
 echo.
